5e6bc785a1ac6b7842a7b00e8b99ec0f6af09b92
humphreyotieno1 <humphreyotieno04@gmail.com>
2024-11-03 12:09:41 +0300

    Taking in user input

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# Program to take user input and display a personalized message
+name = input("Please enter your name: ")
+age = input("Please enter your age: ")
+location = input("Please enter your the location you reside in: ")
+
+print(f"Hello {name}, you are {age} years old and live in {location}.")
